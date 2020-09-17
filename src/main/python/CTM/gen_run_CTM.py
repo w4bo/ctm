914f88faa9fdc66916a28441f8f2468066e23304
mfrancia <matteo.francia2@studio.unibo.it>
2020-09-17 15:04:23 +0200

    ready for tests

--- src/main/python/CTM/gen_run_CTM.py
+++ src/main/python/CTM/gen_run_CTM.py
@@ -5,6 +5,38 @@ import os
 
 filename = "../../../../run_CTM.sh"
 datasets = ["trajectory.milan_standard", "trajectory.geolife_standard"]  # "trajectory.oldenburg_standard",
+configs = {
+    "co-location": {
+        "epss": {"values": ["3", "5", "Infinity"], "default": "Infinity"},
+        "epst": {"values": ["Infinity"], "default": "Infinity"},
+        "platoon" : {"values": ["true"], "default": "false"},
+        "timescale": {"values": ["notime"], "default": "notime"}
+    },
+    "flow": {
+        "epss": {"values": ["1"], "default": "1"},
+        "epst": {"values": ["Infinity"], "default": "Infinity"},
+        "platoon" : {"values": ["true"], "default": "false"},
+        "timescale": {"values": ["notime"], "default": "notime"}
+    },
+    "swarm": {
+        "epss": {"values": ["3", "5", "Infinity"], "default": "Infinity"},
+        "epst": {"values": ["Infinity"], "default": "Infinity"},
+        "platoon" : {"values": ["true"], "default": "false"},
+        "timescale":    {"values": ["daily"], "default": "daily"},  # ,"absolute", "weekly"
+    },
+    "flock": {
+        "epss": {"values": ["3", "5", "Infinity"], "default": "Infinity"},
+        "epst": {"values": ["1"], "default": "1"},
+        "platoon" : {"values": ["true"], "default": "false"},
+        "timescale": {"values": ["daily"], "default": "daily"},  # ,"absolute", "weekly"
+    },
+    "platoon": {
+        "epss": {"values": ["3", "5", "Infinity"], "default": "Infinity"},
+        "epst": {"values": ["1"], "default": "1"},
+        "platoon" : {"values": ["true"], "default": "true"},
+        "timescale": {"values": ["daily"], "default": "daily"},  # ,"absolute", "weekly"
+    },
+}
 params = {
     "minsize": {
         "trajectory.geolife_standard": {"values": ["100", "250", "500"], "default": "100"},
@@ -15,11 +47,8 @@ params = {
         "trajectory.geolife_standard": {"values": ["8"], "default": "8"},      # cell size (125m*bins, 125m*bins)
         "trajectory.milan_standard":   {"values": ["15"], "default": "15"},
     },
-    "epss":         {"values": ["1", "3", "5", "Infinity"], "default": "3"},
-    "timescale":    {"values": ["daily", "notime"], "default": "daily"},  # ,"absolute", "weekly"
     "unitt":        {"values": ["3600"], "default": "3600"},
-    "bint":         {"values": ["6"], "default": "6"},
-    "epst":         {"values": ["1"], "default": "1"},
+    "bint":         {"values": ["2"], "default": "2"},
     "nexecutors":   {"values": ["10"], "default": "10"},
     "ncores":       {"values": ["3"], "default": "3"},
     "maxram":       {"values": ["20g"], "default": "20g"},
@@ -30,6 +59,11 @@ params = {
         "trajectory.milan_standard":   {"values": ["100000", "1000000", "10000000"], "default": "10000000"},
     },
 }
+
+default_params = {}
+for ckey, config in configs.items():
+    for key, value in params.items():
+        config[key] = value
 
 # While testing `key`, do not run configurations including `values`
 exclude = {
@@ -44,34 +78,35 @@ def giveExecutionPermissionToFile(path):
 runs = []
 with codecs.open(filename, "w", "utf-8") as w:
     for dataset in datasets:
-        for key, value in params.items():
-            values = value["values"] if not dataset in value else value[dataset]["values"]
-            if len(values) == 1: # useless to iterate on parameters with a single value, these are already tested
-                continue
-            for v in values:
-                s = " --tbl=" + dataset + (" --euclidean" if "oldenburg" in dataset else "")
-                s += " --{key}={value}".format(key=key, value=v)
-                to_exclude = False
-                for ikey, ivalue in params.items():
-                    default = ivalue["default"] if not dataset in ivalue else ivalue[dataset]["default"]
-                    if key in exclude and default in exclude[key]: # do not generate excluded configurations
-                        to_exclude = True
-                        continue
-                    if key != ikey:
-                        s += " --{key}={value}".format(key=ikey, value=default)
-                print(s)
-                # See https://docs.oracle.com/javase/8/docs/technotes/guides/rmi/javarmiproperties.html
-                # spark.driver.extraJavaOptions
-                # -Djava.rmi.server.hostname=isi-bigcluster8
-                s = "spark-submit --conf \"spark.driver.extraJavaOptions=-Dlog4jspark.root.logger=WARN,console\" " \
-                                    "--conf \"spark.executor.extraJavaOptions=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 " \
-                                            "-Dcom.sun.management.jmxremote.rmi.port=8091 -Dcom.sun.management.jmxremote.authenticate=false " \
-                                            "-Dcom.sun.management.jmxremote.ssl=false \" " \
-                                    "--conf spark.memory.fraction=0.8 " \
-                                    "--conf spark.driver.maxResultSize=10g --driver-memory 20G " \
-                                    "--master yarn --deploy-mode client " \
-                                    "--class it.unibo.big.CTM build/libs/CTM-all.jar" + s
-                if not to_exclude:
+        for config in configs:
+            for key, value in params.items():
+                values = value["values"] if not dataset in value else value[dataset]["values"]
+                if len(values) == 1: # useless to iterate on parameters with a single value, these are already tested
+                    continue
+                for v in values:
+                    s = " --tbl=" + dataset + (" --euclidean" if "oldenburg" in dataset else "")
+                    s += " --{key}={value}".format(key=key, value=v)
+                    to_exclude = False
+                    for ikey, ivalue in params.items():
+                        default = ivalue["default"] if not dataset in ivalue else ivalue[dataset]["default"]
+                        if key in exclude and default in exclude[key]: # do not generate excluded configurations
+                            to_exclude = True
+                            continue
+                        if key != ikey:
+                            s += " --{key}={value}".format(key=ikey, value=default)
+                    print(s)
+                    # See https://docs.oracle.com/javase/8/docs/technotes/guides/rmi/javarmiproperties.html
+                    # spark.driver.extraJavaOptions
+                    # -Djava.rmi.server.hostname=isi-bigcluster8
+                    s = "spark-submit --conf \"spark.driver.extraJavaOptions=-Dlog4jspark.root.logger=WARN,console\" " \
+                                        "--conf \"spark.executor.extraJavaOptions=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8090 " \
+                                                "-Dcom.sun.management.jmxremote.rmi.port=8091 -Dcom.sun.management.jmxremote.authenticate=false " \
+                                                "-Dcom.sun.management.jmxremote.ssl=false \" " \
+                                        "--conf spark.memory.fraction=0.8 " \
+                                        "--conf spark.driver.maxResultSize=10g --driver-memory 20G " \
+                                        "--master yarn --deploy-mode client " \
+                                        "--class it.unibo.big.CTM build/libs/CTM-all.jar" + s
+                    # if not to_exclude:
                     runs.append(s)
     w.write('\n'.join(runs))
 
