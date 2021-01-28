3b067ddd5afa63e671bab3dc575e6d3f60fb68ed
mfrancia <matteo.francia2@studio.unibo.it>
2021-01-28 13:33:42 +0100

    trying new settings

--- src/main/scala/it/unibo/big/CTM2.scala
+++ src/main/scala/it/unibo/big/CTM2.scala
@@ -202,8 +202,8 @@ object CTM2 {
               val conncomp = connectedComponent(R, RoaringBitmap.or(XplusY, R), isPlatoon = false, returnComponents = true)
               // R.toArray()
               if (conncomp._1 >= minsup) {
-                R = RoaringBitmap.and(R, conncomp._2)
-                R.forEach(toJavaConsumer({ key: Integer => {
+                // R = RoaringBitmap.and(R, conncomp._2)
+                RoaringBitmap.and(R, conncomp._2).forEach(toJavaConsumer({ key: Integer => {
                   val c = RoaringBitmap.and(lCluster, brdTrajInCell.value(key)) // new co-movement pattern
                   R = RoaringBitmap.remove(R, key, key + 1) // reduce the search space
                   val XplusYplusKey = RoaringBitmap.add(XplusY, key, key + 1) // update the current support
