a6102b232a1b578afddb9a5f27eacdbb4816db60
mfrancia <matteo.francia2@studio.unibo.it>
2021-01-29 16:31:54 +0100

    add query mechanism

--- src/main/scala/it/unibo/big/Query.scala
+++ src/main/scala/it/unibo/big/Query.scala
@@ -11,7 +11,11 @@ object Query {
      * @param bin_s   bin_s \in N (1, ..., n). Multiply cell size 123m x bin_s
      */
     def run(inTable: String, minsize: Int, minsup: Int, bin_s: Int): Unit = {
-        val sparkSession = SparkSession.builder().appName("Query trajectory").master("yarn").enableHiveSupport.getOrCreate
+        val sparkSession = SparkSession.builder()
+            .appName("Query trajectory")
+            .master("yarn")
+            .config("spark.sql.legacy.allowCreatingManagedTableUsingNonemptyLocation", "true")
+            .enableHiveSupport.getOrCreate
         val sql =
             s"""select i.itemsetid, t.itemid, t.tid, s.userid, s.trajectoryid, s.`timestamp`, s.latitude, s.longitude, t.latitude as bin_latitude, t.longitude as bin_longitude, t.time_bucket * 3600 as bin_timestamp, u.tileid as in_support
                |from (select itemsetid, support from ctm.CTM__tbl_${inTable}__lmt_10000000__size_${minsize}__sup_${minsup}__bins_${bin_s}__ts_notime__bint_1__unitt_3600__epss_Infinity__epst_Infinity__freq_1__sthr_1000000__summary order by 2 desc limit 10) a
